d2e4980d7228bb8c39428d8df4fe2d004260a11b
dharneeshgupta <dharneeshgupta178@gmail.com>
2020-11-26 20:36:22 +0530

    First commit:create the directory structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+
+const app=express();
+
+app.listen(8080,(err)=>{
+
+    if(err)
+    {
+        console.log("error in running up the server",err);
+        return;
+    }
+
+    console.log("severs is up & running on port 8080");
+});
+
