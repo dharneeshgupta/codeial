926afe943e4a762f6f48d9190e570bbc98960008
dharneeshgupta <dharneeshgupta178@gmail.com>
2020-11-26 22:57:27 +0530

    added user router and its controller

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router=express.Router();
 
 const homeController=require('../controllers/home_controller');
 router.get('/',homeController.home);
+
+router.use('/users',require('./users'));
 console.log("routers loaded");
 
 
