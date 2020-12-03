7db4f00bc8240aec4428effd8adf18b09fbce96d
dharneeshgupta <dharneeshgupta178@gmail.com>
2020-12-03 10:43:56 +0530

    setted up the view engine

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require('express');
-
+const path=require('path');
 const app=express();
 
+
+//setup the view engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'./views'));
 
 app.use('/',require('./routes/index.js'));
 app.listen(8080,(err)=>{
